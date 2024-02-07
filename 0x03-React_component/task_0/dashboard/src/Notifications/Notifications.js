03ad07601f9e2196d0a17c34c06c7bd11f209367
Abdulrahman Moussa <abdelrahmanmoussa111@gmail.com>
2024-02-07 11:07:34 +0200

    0x03

---
+++ 0x03-React_component/task_0/dashboard/src/Notifications/Notifications.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import closeIcon from '../assets/close-icon.png';
+import NotificationItem from './NotificationItem';
+import NotificationItemShape from './NotificationItemShape';
+import './Notifications.css';
+
+function Notifications({ displayDrawer, listNotifications }) {
+  return (
+    <div className="notificationsWrapper">
+      <div className="menuItem">Your notifications</div>
+      {displayDrawer && (
+        <div className="Notifications">
+          <button
+            aria-label="Close"
+            onClick={() => {
+              console.log('Close button has been clicked');
+            }}
+            style={{
+              position: 'absolute',
+              right: '1rem',
+              border: 'none',
+              cursor: 'pointer',
+            }}
+          >
+            <img src={closeIcon} alt="Close" />
+          </button>
+          <p>Here is the list of notifications</p>
+          <ul>
+            {listNotifications.length === 0 ? (
+              <NotificationItem
+                type="default"
+                value="No new notification for now"
+              />
+            ) : (
+              listNotifications.map((notification) => (
+                <NotificationItem
+                  key={notification.id}
+                  type={notification.type}
+                  value={notification.value}
+                  html={notification.html}
+                />
+              ))
+            )}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+}
+
+Notifications.propTypes = {
+  displayDrawer: PropTypes.bool,
+  listNotifications: PropTypes.arrayOf(NotificationItemShape),
+};
+
+Notifications.defaultProps = {
+  displayDrawer: false,
+};
+
+export default Notifications;
