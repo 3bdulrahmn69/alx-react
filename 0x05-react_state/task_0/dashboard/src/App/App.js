82e2444b4a158df72575f64f9cc8307619089e90
Abdulrahman Moussa <abdelrahmanmoussa111@gmail.com>
2024-02-17 21:30:08 +0200

    0x05;

--- 0x05-react_state/task_0/dashboard/src/App/App.js
+++ 0x05-react_state/task_0/dashboard/src/App/App.js
@@ -1,51 +1,44 @@
-import React from 'react';
-import Header from '../Header/Header';
-import Footer from '../Footer/Footer';
-import Login from '../Login/Login';
-import CourseList from '../CourseList/CourseList';
-import Notifications from '../Notifications/Notifications';
-import BodySectionWithMarginBottom from '../BodySection/BodySectionWithMarginBottom';
-import BodySection from '../BodySection/BodySection';
-import { StyleSheet, css } from 'aphrodite';
-import PropTypes from 'prop-types';
-import { getLatestNotification } from '../utils/utils';
+import React from "react";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import Login from "../Login/Login";
+import CourseList from "../CourseList/CourseList";
+import Notifications from "../Notifications/Notifications";
+import BodySectionWithMarginBottom from "../BodySection/BodySectionWithMarginBottom";
+import BodySection from "../BodySection/BodySection";
+import { StyleSheet, css } from "aphrodite";
+import PropTypes from "prop-types";
+import { getLatestNotification } from "../utils/utils";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
-    this.handleKeyPress = this.handleKeyPress.bind(this);
-
     this.state = { displayDrawer: false };
+
+    this.handleKeyPress = this.handleKeyPress.bind(this);
     this.handleDisplayDrawer = this.handleDisplayDrawer.bind(this);
     this.handleHideDrawer = this.handleHideDrawer.bind(this);
   }
 
   listCourses = [
-    { id: 1, name: 'ES6', credit: 60 },
-    { id: 2, name: 'Webpack', credit: 20 },
-    { id: 3, name: 'React', credit: 40 },
+    { id: 1, name: "ES6", credit: 60 },
+    { id: 2, name: "Webpack", credit: 20 },
+    { id: 3, name: "React", credit: 40 },
   ];
 
   listNotifications = [
-    { id: 1, type: 'default', value: 'New course available' },
-    { id: 2, type: 'urgent', value: 'New resume available' },
-    { id: 3, type: 'urgent', html: getLatestNotification() },
+    { id: 1, type: "default", value: "New course available" },
+    { id: 2, type: "urgent", value: "New resume available" },
+    { id: 3, type: "urgent", html: getLatestNotification() },
   ];
 
   handleKeyPress(e) {
-    if (e.ctrlKey && e.key === 'h') {
+    if (e.ctrlKey && e.key === "h") {
       e.preventDefault();
-      alert('Logging you out');
+      alert("Logging you out");
       this.props.logOut();
     }
-  }
-  componentDidMount() {
-    document.addEventListener('keydown', this.handleKeyPress);
-  }
-
-  componentWillUnmount() {
-    document.removeEventListener('keydown', this.handleKeyPress);
   }
 
   handleDisplayDrawer() {
@@ -54,6 +47,14 @@ class App extends React.Component {
 
   handleHideDrawer() {
     this.setState({ displayDrawer: false });
+  }
+
+  componentDidMount() {
+    document.addEventListener("keydown", this.handleKeyPress);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener("keydown", this.handleKeyPress);
   }
 
   render() {
@@ -80,10 +81,8 @@ class App extends React.Component {
           )}
           <BodySection title="News from the school">
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Perspiciatis at tempora odio, necessitatibus repudiandae
-              reiciendis cum nemo sed asperiores ut molestiae eaque aliquam illo
-              ipsa iste vero dolor voluptates.
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis at tempora odio, necessitatibus repudiandae reiciendis cum nemo sed asperiores ut molestiae eaque aliquam illo ipsa
+              iste vero dolor voluptates.
             </p>
           </BodySection>
           <Footer />
@@ -95,10 +94,10 @@ class App extends React.Component {
 
 const styles = StyleSheet.create({
   App: {
-    height: '100vh',
-    maxWidth: '100vw',
-    position: 'relative',
-    fontFamily: 'Arial, Helvetica, sans-serif',
+    height: "100vh",
+    maxWidth: "100vw",
+    position: "relative",
+    fontFamily: "Arial, Helvetica, sans-serif",
   },
 });
 
--- 0x05-react_state/task_1/dashboard/src/Login/Login.js
+++ 0x05-react_state/task_1/dashboard/src/Login/Login.js
@@ -1,17 +1,58 @@
-import React from "react";
-import { StyleSheet, css } from "aphrodite";
+import React, { useEffect } from 'react';
+import { StyleSheet, css } from 'aphrodite';
+import { useState } from 'react';
 
 function Login() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [enableSubmit, setEnableSubmit] = useState(false);
+
+  const handleLoginSubmit = (e) => {
+    e.preventDefault();
+    setIsLoggedIn(true);
+  };
+
+  const handleChangeEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handleChangePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  useEffect(() => {
+    if (email.length > 0 && password.length > 0) {
+      setEnableSubmit(true);
+    } else {
+      setEnableSubmit(false);
+    }
+  }, [email, password]);
+
   return (
     <React.Fragment>
-      <div className={css(styles["App-body"])}>
+      <div className={css(styles['App-body'])}>
         <p>Login to access the full dashboard</p>
-        <form>
+        <form onSubmit={handleLoginSubmit}>
           <label htmlFor="email">Email:</label>
-          <input className={css(styles.input)} type="email" name="email"></input>
+          <input
+            className={css(styles.input)}
+            id="email"
+            type="email"
+            name="email"
+            value={email}
+            onChange={handleChangeEmail}
+          ></input>
           <label htmlFor="password">Password:</label>
-          <input className={css(styles.input)} type="password" name="password"></input>
-          <button>OK</button>
+          <input
+            className={css(styles.input)}
+            id="password"
+            type="password"
+            name="password"
+            value={password}
+            onChange={handleChangePassword}
+          ></input>
+          <input type="submit" value="Ok" disabled={!enableSubmit}></input>
         </form>
       </div>
     </React.Fragment>
@@ -19,18 +60,18 @@ function Login() {
 }
 
 const styles = StyleSheet.create({
-  "App-body": {
-    fontSize: "1rem",
-    padding: "2em",
-    height: "45%",
-    "@media (max-width: 900px)": {
-      display: "flex",
-      flexDirection: "column",
+  'App-body': {
+    fontSize: '1rem',
+    padding: '2em',
+    height: '45%',
+    '@media (max-width: 900px)': {
+      display: 'flex',
+      flexDirection: 'column',
     },
   },
 
   input: {
-    margin: "10px",
+    margin: '10px',
   },
 });
 
